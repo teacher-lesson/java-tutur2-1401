d8b5e4193e249df02b21cadab21427a59dc95571
Mohsen <mohsenr7596@hotmail.com>
2023-05-31 20:47:46 +0330

    add read, write from map

--- src/main/java/com/example/section2/WriteBinaryToFile.java
+++ src/main/java/com/example/section2/WriteBinaryToFile.java
@@ -3,15 +3,21 @@ package com.example.section2;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.Map;
 
 public class WriteBinaryToFile {
 
     public static void main(String[] args) throws IOException {
-        final ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("./stu"));
+        final ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("./data"));
 
-        final Student ali = new Student("ali", "123456789", 20);
+        Map<Long, Student> map = Map.of(
+                1000L , new Student("Ali", 20),
+                1001L , new Student("Hassan", 21),
+                1002L , new Student("HosseinAli", 22),
+                1003L , new Student("Reza", 19)
+        );
 
-        outputStream.writeObject(ali);
+        outputStream.writeObject(map);
 
         outputStream.close();
     }
