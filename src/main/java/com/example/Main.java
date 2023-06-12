3da712a19b205d2493547ecbb6f5e55552b27047
Mohsen <mohsenr7596@hotmail.com>
2023-06-12 14:40:09 +0330

    Joption pane

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,8 +1,14 @@
 package com.example;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        final String year = JOptionPane.showInputDialog("Input your birthday year");
+
+        final int age = 2023 - Integer.parseInt(year);
+
+        JOptionPane.showMessageDialog(null, String.format("Your age is %d", age));
     }
 }
